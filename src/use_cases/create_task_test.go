ed8a0b43870381e1001e8a147eb1e4fe4ca167eb
NitzanShwartz <nitzan.shwartz12@gmail.com>
2025-01-14 15:25:09 +0200

    modified: changed create task tests to align with the extraction of notification logic in the previous commit

--- src/use_cases/create_task_test.go
+++ src/use_cases/create_task_test.go
@@ -22,14 +22,6 @@ func (tr *TaskRepositoryMock) CreateTask(task entities.Task) error {
 	return tr.CreateTaskMock(task)
 }
 
-type TaskNotificationProviderMock struct {
-	SendNotificationMock func(task entities.Task) error
-}
-
-func (tn *TaskNotificationProviderMock) SendNotification(task entities.Task) error {
-	return tn.SendNotificationMock(task)
-}
-
 func TestCreateTaskSuccess(t *testing.T) {
 	repositoryMock := &TaskRepositoryMock{
 		DoesTaskExistsMock: func(title string) bool {
@@ -40,13 +32,7 @@ func TestCreateTaskSuccess(t *testing.T) {
 		},
 	}
 
-	notifierMock := &TaskNotificationProviderMock{
-		SendNotificationMock: func(task entities.Task) error {
-			return nil
-		},
-	}
-
-	useCase := usecases.NewCreateTask(repositoryMock, notifierMock)
+	useCase := usecases.NewCreateTask(repositoryMock)
 	err := useCase.Execute("test task", "test task description", "name@email.com")
 	if err != nil {
 		t.Error("this function call should not fail")
@@ -63,13 +49,7 @@ func TestCreateTaskFailNoTaskName(t *testing.T) {
 		},
 	}
 
-	notifierMock := &TaskNotificationProviderMock{
-		SendNotificationMock: func(task entities.Task) error {
-			return nil
-		},
-	}
-
-	useCase := usecases.NewCreateTask(repositoryMock, notifierMock)
+	useCase := usecases.NewCreateTask(repositoryMock)
 	err := useCase.Execute("", "test task description", "name@email.com")
 	if err == nil {
 		t.Error("this function call should fail, missing name for task!")
@@ -86,13 +66,7 @@ func TestCreateTaskFailNoTaskDescription(t *testing.T) {
 		},
 	}
 
-	notifierMock := &TaskNotificationProviderMock{
-		SendNotificationMock: func(task entities.Task) error {
-			return nil
-		},
-	}
-
-	useCase := usecases.NewCreateTask(repositoryMock, notifierMock)
+	useCase := usecases.NewCreateTask(repositoryMock)
 	err := useCase.Execute("test task", "", "name@email.com")
 	if err == nil {
 		t.Error("this function call should fail, missing name for task!")
@@ -109,13 +83,7 @@ func TestCreateTaskFailNoTaskUserEmail(t *testing.T) {
 		},
 	}
 
-	notifierMock := &TaskNotificationProviderMock{
-		SendNotificationMock: func(task entities.Task) error {
-			return nil
-		},
-	}
-
-	useCase := usecases.NewCreateTask(repositoryMock, notifierMock)
+	useCase := usecases.NewCreateTask(repositoryMock)
 	err := useCase.Execute("test task", "test task description", "")
 	if err == nil {
 		t.Error("this function call should fail, missing name for task!")
@@ -132,13 +100,7 @@ func TestCreateTaskFailTaskUserEmailDoesNotMatchConvention(t *testing.T) {
 		},
 	}
 
-	notifierMock := &TaskNotificationProviderMock{
-		SendNotificationMock: func(task entities.Task) error {
-			return nil
-		},
-	}
-
-	useCase := usecases.NewCreateTask(repositoryMock, notifierMock)
+	useCase := usecases.NewCreateTask(repositoryMock)
 	err := useCase.Execute("test task", "test task description", "nameemail.com")
 	if err == nil {
 		t.Error("this function call should fail, missing name for task!")
@@ -155,13 +117,7 @@ func TestCreateTaskFailTaskRepositoryCreateFailed(t *testing.T) {
 		},
 	}
 
-	notifierMock := &TaskNotificationProviderMock{
-		SendNotificationMock: func(task entities.Task) error {
-			return nil
-		},
-	}
-
-	useCase := usecases.NewCreateTask(repositoryMock, notifierMock)
+	useCase := usecases.NewCreateTask(repositoryMock)
 	err := useCase.Execute("test task", "test task description", "name@email.com")
 	if err.Error() != "test error" {
 		t.Errorf("this function call should fail, and an error should be raised with the message 'test error', error: %v", err.Error())
@@ -178,39 +134,10 @@ func TestCreateTaskFailTaskRepositoryTaskAlreadyExists(t *testing.T) {
 		},
 	}
 
-	notifierMock := &TaskNotificationProviderMock{
-		SendNotificationMock: func(task entities.Task) error {
-			return nil
-		},
-	}
-
-	useCase := usecases.NewCreateTask(repositoryMock, notifierMock)
+	useCase := usecases.NewCreateTask(repositoryMock)
 	err := useCase.Execute("test task", "test task description", "name@email.com")
 	var tae *exceptions.TaskAlreadyExistsError
 	if !errors.As(err, &tae) {
-		t.Errorf("this function call should fail, and an error should be raised with the message 'test error', error: %v", err.Error())
-	}
-}
-
-func TestCreateTaskFailTaskNotificationFailed(t *testing.T) {
-	repositoryMock := &TaskRepositoryMock{
-		DoesTaskExistsMock: func(title string) bool {
-			return false
-		},
-		CreateTaskMock: func(task entities.Task) error {
-			return nil
-		},
-	}
-
-	notifierMock := &TaskNotificationProviderMock{
-		SendNotificationMock: func(task entities.Task) error {
-			return errors.New("test error")
-		},
-	}
-
-	useCase := usecases.NewCreateTask(repositoryMock, notifierMock)
-	err := useCase.Execute("test task", "test task description", "name@email.com")
-	if err.Error() != "test error" {
 		t.Errorf("this function call should fail, and an error should be raised with the message 'test error', error: %v", err.Error())
 	}
 }
