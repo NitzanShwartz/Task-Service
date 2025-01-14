f1ed860ff13a62e58baf8a840c92cd2c8e8e3f03
NitzanShwartz <nitzan.shwartz12@gmail.com>
2025-01-14 18:34:27 +0200

    modified: new task returns a copy of the struct instead of a pointer since the struct is small there is not benefit to use a pointer

--- src/entities/task.go
+++ src/entities/task.go
@@ -10,14 +10,18 @@ type Task struct {
 	UserEmail   string
 }
 
-func NewTask(title string, description string, userEmail string) (*Task, error) {
+func NewTask(title string, description string, userEmail string) (Task, error) {
 	if title == "" || description == "" || userEmail == "" {
-		return nil, errors.New("title, task and userEmail are mandatory fields")
+		return Task{}, errors.New("title, task and userEmail are mandatory fields")
 	}
 
-	return &Task{
+	return Task{
 		Title:       title,
 		Description: description,
 		UserEmail:   userEmail,
 	}, nil
+}
+
+func NewEmptyTask() Task {
+	return Task{}
 }
