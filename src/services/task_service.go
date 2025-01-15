eff33c8a43e32639f51161c6f7afe9819982ae81
NitzanShwartz <nitzan.shwartz12@gmail.com>
2025-01-15 12:50:27 +0200

    modified: the struct now holds the use cases + CreateTask implementation for the service

--- src/services/task_service.go
+++ src/services/task_service.go
@@ -1,16 +1,32 @@
 package services
 
-import "github.com/NitzanShwartz/Task-Service/src/repositories"
+import (
+	"github.com/NitzanShwartz/Task-Service/src/repositories"
+	usecases "github.com/NitzanShwartz/Task-Service/src/use_cases"
+)
 
 type TaskService struct {
-	TaskRepository         repositories.TaskRepository
-	NotificationRepository repositories.TaskNotificationProvider
+	CreateTaskUseCase usecases.CreateTask
+	NotifyTaskUseCase usecases.NotifyTask
 }
 
 func NewTaskService(taskRepository repositories.TaskRepository, notificationRepository repositories.TaskNotificationProvider) *TaskService {
 	return &TaskService{
-		TaskRepository:         taskRepository,
-		NotificationRepository: notificationRepository,
+		CreateTaskUseCase: usecases.CreateTask{
+			TaskRepository: taskRepository,
+		},
+		NotifyTaskUseCase: usecases.NotifyTask{
+			NotificationRepository: notificationRepository,
+		},
 	}
 }
 
+func (ts *TaskService) CreateTask(title string, description string, userEmail string) error {
+	task, err := ts.CreateTaskUseCase.Execute(title, description, userEmail)
+	if err != nil {
+		return err
+	}
+
+	err = ts.NotifyTaskUseCase.Execute(task)
+	return err
+}
